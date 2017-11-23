cbb898321871305e37bae9b70fde228e7a40ba4f
thepillowthief <apocalypsefactory@gmail.com>
2017-11-22 21:44:57 -0700

    Barebones game structure assembling

---
+++ main.lua
@@ -0,0 +1,13 @@
+Gamestate = require "lib/hump/gamestate"
+
+function love.load()
+  love.graphics.setDefaultFilter( 'nearest', 'nearest' )
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
